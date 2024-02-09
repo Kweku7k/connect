25b201c1c8a137eed32ccc8306df1d02e33d8c74
Kweku7k <mr.adumatta@gmail.com>
2024-02-09 10:52:33 +0000

    upload

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ source $venv_name/bin/activate
 pip install -r requirements.txt
 
 # Install uwsgi
-pip install uwsgi
+# pip install uwsgi
 
 #Restart the forms application
 sudo systemctl restart forms.service
