fc10f75199c0cbde3f74a05e1e147e6ae8272710
Kweku7k <mr.adumatta@gmail.com>
2024-03-04 20:35:48 +0000

    added email sender

--- app.py
+++ app.py
@@ -1253,12 +1253,21 @@ def internal_server_error(error):
 
 @app.route('/foomail', methods=['GET', 'POST'])
 def foomail():
-    return sendAnEmail('Testing', 'Testing', 'Hello', ['mr.adumatta@gmail.com'])
-     
+    if request.method == 'POST':
+        body = request.json
+        pprint.pprint(body)
+
+        templateId = body.get("templateId")
+    
+        html_content =  render_template(f'email/{templateId}.html', body = body.get("templateBody") )
+        return sendAnEmail(body.get("title"), body.get("subject"), html_content, body.get("receivers"))
+    else:
+        return "Bruh."
+
 
 # send an email
 
-def sendAnEmail(title, subject, message, email_receiver, path=None):
+def sendAnEmail(title, subject, html_content, email_receiver, path=None):
     print("Attempting to send an email")
     print(email_receiver)
     print(type(email_receiver))
@@ -1266,50 +1275,6 @@ def sendAnEmail(title, subject, message, email_receiver, path=None):
     email_sender = os.environ["PRESTO_MAIL_USERNAME"]
     email_password = os.environ["PRESTO_MAIL_PASSWORD"]
 
-
-    html_content = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <style>
-        @font-face {{
-            font-family: 'Plus Jakarta';
-            src: url('PlusJakartaSans-VariableFont_wght.woff2') format('woff2-variations'),
-                url('PlusJakartaSans-Italic-VariableFont_wght.woff2') format('woff2-variations');
-            font-weight: 100 500; /* Adjust font weights based on available weights */
-            font-style: normal;
-        }}
-
-        body {{
-            font-family: 'Plus Jakarta', sans-serif;
-            color: #000;
-            margin: auto 5vw;
-        }}
-
-        div{{
-            font-family: 'Plus Jakarta', sans-serif;
-            font-weight:400;
-        }}
-
-
-        </style>
-
-    </head>
-    <body style="margin:auto 5vw; color:black; font-family: 'Plus Jakarta', sans-serif; font-weight:400;">
-
-        
-        <!-- Your banner image above -->
-
-
-        <div style="font-family:'Poppins', sans serif; font-weight: 400; font-size: 20px; line-height:26px; color: #000;">
-            {message}
-        </div>
-
-
-        <h6 style="font-weight:200; font-size: 14px;">This email is powered by <a href='https://prestoghana.com'>PrestoGhana</a></h6>
-    </body>
-    </html>
-    """
 
     em = EmailMessage()
     em["From"] = f"{title} <{email_sender}>"
